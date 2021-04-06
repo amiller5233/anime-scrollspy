b01cc3c7e2e6063228f9455870a8facae2dc3d78
Adam J. Miller <amiller5233@gmail.com>
2021-04-06 10:44:13 -0700

    Add files via upload

---
+++ anime-scrollspy.js
@@ -0,0 +1,35 @@
+class AnimeScrollspy {
+	constructor(threshold=0.8) {
+		this.threshold = threshold
+		this.elements = []
+	}
+
+	add(target, anime) {
+		this.elements.push(new AnimeScrollspyElement(target, anime))
+	}
+
+	scroll() {
+		var threshold = this.threshold
+		this.elements.forEach(function(el){
+			if(el.scrollTop < window.scrollY+window.innerHeight*threshold && !el.anime.began) {
+				el.anime.play()
+			}
+		})
+	}
+
+}
+
+class AnimeScrollspyElement {
+	constructor(target, anime) {
+		this.target = target;
+		this.anime = anime;
+	}
+
+	get scrollTop() {
+		return $(this.target).position().top
+	}
+}
+
+const anime_scrollspy = new AnimeScrollspy()
+
+document.addEventListener('scroll', function(e){ anime_scrollspy.scroll() })
